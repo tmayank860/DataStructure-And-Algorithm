bc0798135651f33bc4c599cc7636a6872bed65f9
tmayank860 <tmayank860@gmail.com>
2019-12-03 23:49:21 +0530

    .

--- src/recursion/Fibonacci.java
+++ src/recursion/Fibonacci.java
@@ -2,11 +2,15 @@ package recursion;
 
 public class Fibonacci {
 	public static void main(String[] args) {
-		System.out.println(fib(6));
+
+		System.out.println(fib(10));
+		for(int i=0;i<10;i++){
+			System.out.print(fib(i)+" ");
+		}
 	}
 
 	public static int fib(int n) {
-		if (n == 0 || n == 1) {
+		if (n <=1) {
 			return n;
 		} else {
 			return fib(n - 1) + fib(n - 2);
