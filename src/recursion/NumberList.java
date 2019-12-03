a68abfe72dbe547654a7c5dd467f658a8313be8d
tmayank860 <tmayank860@gmail.com>
2019-12-03 22:38:46 +0530

    .

--- src/recursion/NumberList.java
+++ src/recursion/NumberList.java
@@ -3,8 +3,12 @@ package recursion;
 public class NumberList {
     public static void main(String[] args) {
     System.out.println("Hello");
+    PDI(5);
+    PD(5);
     PI(5);
     }
+
+//    Print decreasing
     public static void PD(int n){
         if (n==0){
             return;
@@ -13,12 +17,24 @@ public class NumberList {
         System.out.println(n);
         PD(n-1);
     }
+//    Print Increasing
     public static void PI(int n){
         if (n==0){
             return;
         }
 
         PI(n-1);
+        System.out.println(n);
+    }
+
+//    Print Increasing and Decreasing
+
+    public static void PDI(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+        PDI(n-1);
         System.out.println(n);
     }
 
