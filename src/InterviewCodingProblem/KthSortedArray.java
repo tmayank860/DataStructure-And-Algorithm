abefc32aaa511629d3253a54227d997a5d20534e
tmayank860 <tmayank860@gmail.com>
2020-03-29 12:32:02 +0530

    .

--- src/InterviewCodingProblem/KthSortedArray.java
+++ src/InterviewCodingProblem/KthSortedArray.java
@@ -1,10 +1,48 @@
 package InterviewCodingProblem;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class KthSortedArray {
     public static void main (String[] args) {
+
+//        METHOD 1
+
+//        Scanner sc=new Scanner(System.in);
+//        int t=sc.nextInt();
+//        while(t--!=0){
+//            int n,k;
+//            n=sc.nextInt();
+//            k=sc.nextInt();
+//            PriorityQueue<Integer> pq=new PriorityQueue<>();
+//            int[] arr=new int[n];
+//            for(int i=0;i<n;i++){
+//                arr[i]=sc.nextInt();
+//            }
+//            for(int i=0;i<k+1;i++){
+//                pq.add(arr[i]);
+//            }int j=0;
+//            for(int i=k+1;i<n;i++){
+//                arr[j]=pq.poll();
+//                pq.add(arr[i]);
+//                j++;
+//            }
+//            while(!pq.isEmpty()){
+//                arr[j]=pq.poll();
+//                j++;
+//            }
+//            for(int i=0;i<n;i++){
+//                System.out.print(arr[i]+" ");
+//            }
+//            System.out.println();
+//
+//        }
+//
+
+
+
+
         Scanner sc=new Scanner(System.in);
         int t;
         t=sc.nextInt();
@@ -35,3 +73,4 @@ public class KthSortedArray {
         }
     }
 }
+
