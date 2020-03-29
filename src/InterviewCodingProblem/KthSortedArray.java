b1d09f2baa87f49d483ef9fe58a1a9b36baac163
tmayank860 <tmayank860@gmail.com>
2020-03-29 12:26:19 +0530

    .

--- src/InterviewCodingProblem/KthSortedArray.java
+++ src/InterviewCodingProblem/KthSortedArray.java
@@ -6,23 +6,32 @@ import java.util.Scanner;
 public class KthSortedArray {
     public static void main (String[] args) {
         Scanner sc=new Scanner(System.in);
-        int t=sc.nextInt();
-        while(t-->0)
-        {
-            int n=sc.nextInt();
-            int k=sc.nextInt();
-            int arr[]=new int[n];
-            for(int i=0;i<n;i++)
-            {
+        int t;
+        t=sc.nextInt();
+        while(t--!=0){
+            int n,p;
+            n=sc.nextInt();
+            p=sc.nextInt();
+            int[] arr=new int[100];
+            for(int i=0;i<n;i++){
                 arr[i]=sc.nextInt();
             }
-            Arrays.sort(arr);
-            for(int i=0;i<n;i++)
-            {
+            //code here
+            int j,k,key;
+            for(j=1;j<n;j++){
+                key=arr[j];
+                k=j-1;
+                while(k>=0&&arr[k]>key){
+                    arr[k+1]=arr[k];
+                    k=k-1;
+                }
+                arr[k+1]=key;
+            }
+
+            for(int i=0;i<n;i++){
                 System.out.print(arr[i]+" ");
             }
             System.out.println();
-
         }
     }
 }
