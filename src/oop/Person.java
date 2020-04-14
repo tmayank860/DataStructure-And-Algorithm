ea5b1bfbf887ca79d7a6d62c272eeae32790fc10
tmayank860 <tmayank860@gmail.com>
2020-04-14 20:07:07 +0530

    .

--- src/oop/Person.java
+++ src/oop/Person.java
@@ -4,6 +4,9 @@ public class Person {
     private String name;
     private int age;
 
+    public Person(){
+        System.out.println("In the constructor");
+    }
     public String getName() {
         return this.name;
     }
