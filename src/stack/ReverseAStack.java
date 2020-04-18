8033f50346cb87966f105e9710fcaa42d95d2f92
tmayank860 <tmayank860@gmail.com>
2020-04-18 22:32:17 +0530

    .

---
+++ src/stack/ReverseAStack.java
@@ -0,0 +1,27 @@
+package stack;
+
+import java.util.Stack;
+
+public class ReverseAStack {
+    public static void main(String[] args) {
+        Stack<Integer> st=new Stack<>();
+        st.push(10);
+        st.push(20);
+        st.push(30);
+        st.push(40);
+        st.push(50);
+        Stack s=reverseStack(st);
+        while (!s.isEmpty()) {
+            System.out.print(s.pop()+"=>");
+        }
+        System.out.println("END");
+    }
+
+    public static Stack<Integer> reverseStack(Stack<Integer> st){
+        Stack<Integer> helperStack=new Stack<>();
+        while (!st.empty()){
+            helperStack.push(st.pop());
+        }
+        return helperStack;
+    }
+}
