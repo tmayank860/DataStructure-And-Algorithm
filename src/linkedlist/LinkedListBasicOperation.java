013c5b2f9eb41952d092058c05d6701b6e5aedf9
tmayank860 <tmayank860@gmail.com>
2020-04-23 22:24:37 +0530

    .

--- src/linkedlist/LinkedListBasicOperation.java
+++ src/linkedlist/LinkedListBasicOperation.java
@@ -1,77 +1,98 @@
 package linkedlist;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Scanner;
-
 public class LinkedListBasicOperation {
-    private class Node{
-        int data;
-        Node next;
-    }
     private Node head;
     private Node tail;
     private int size;
 
-    public static void main(String[] args) throws Exception{
-        LinkedListBasicOperation ls=new LinkedListBasicOperation();
-        ls.insertAtLast(10);
+    public static void main(String[] args) throws Exception {
+        LinkedListBasicOperation ls = new LinkedListBasicOperation();
+        ls.insertAtFirst(10);
         ls.insertAtLast(20);
-        ls.insertAtLast(30);
-        ls.insertAtLast(40);
+        ls.insertAtFirst(30);
+        ls.insertAtFirst(40);
         ls.display();
-        ls.reverse();
-        ls.display();
+        System.out.println();
+        System.out.println(ls.getNodeAt(0).data);
+        System.out.println(ls.getFirst());
+        System.out.println(ls.getLast());
 
     }
 
-    public void display(){
-        Node temp=this.head;
-        while (temp!=null){
-            System.out.print(temp.data+" ");
-            temp=temp.next;
+    public int getFirst() throws Exception {
+        if (size == 0) {
+            throw new Exception("Empty List");
+        }
+        return this.head.data;
+    }
+
+    public int getLast() throws Exception {
+        if (size == 0) {
+            throw new Exception("LL is Empty");
+        }
+        return this.tail.data;
+    }
+
+    public void display() {
+        Node temp = this.head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
         }
     }
-    public void insertAtLast(int value){
-        Node nn=new Node();
-        nn.data=value;
-        nn.next=null;
-        if (this.size>=1){
-            this.tail.next=nn;
+
+    public void insertAtLast(int value) {
+        Node nn = new Node();
+        nn.data = value;
+        nn.next = null;
+        if (this.size >= 1) {
+            this.tail.next = nn;
         }
-        if (size==0){
-            this.head=nn;
-            this.tail=nn;
+        if (size == 0) {
+            this.head = nn;
+            this.tail = nn;
             this.size++;
-        }else {
-            this.tail=nn;
+        } else {
+            this.tail = nn;
             size++;
         }
     }
-    private Node getNodeAt(int index) throws Exception {
-        if (this.size==0){
-            throw new Exception("Empty List");
+
+    public void insertAtFirst(int value) {
+        Node nn = new Node();
+        nn.data = value;
+        nn.next = null;
+        if (size >= 1) {
+            nn.next = head;
         }
-        Node temp=this.head;
-        for (int i=0;i<=index;i++){
-            temp=temp.next;
+        if (size == 0) {
+            this.head = nn;
+            this.tail = nn;
+            size++;
+        } else {
+            this.head = nn;
+            size++;
+        }
+    }
+
+    private Node getNodeAt(int index) throws Exception {
+        if (this.size == 0) {
+            throw new Exception("LinkedList is empty");
+        }
+        if (index < 0 || index >= size) {
+            throw new Exception("Invalid index");
+        }
+        Node temp = this.head;
+        for (int i = 1; i <= index; i++) {
+            temp = temp.next;
         }
         return temp;
     }
-    public void reverse() throws Exception {
-        int left=0;
-        int right=this.size-1;
-        while (left<right){
-            Node ln=getNodeAt(left);
-            Node rn=getNodeAt(right);
-            int x=ln.data;
-            ln.data=rn.data;
-            rn.data=x;
-            left++;
-            right--;
 
-        }
-
+    private class Node {
+        int data;
+        Node next;
     }
+
 
 }
