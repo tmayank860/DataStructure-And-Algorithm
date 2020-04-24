acaec4554d5132ddd856948bc02a1b84d0725b7c
tmayank860 <tmayank860@gmail.com>
2020-04-24 13:09:12 +0530

    .

--- src/linkedlist/LinkedListBasicOperation.java
+++ src/linkedlist/LinkedListBasicOperation.java
@@ -1,9 +1,15 @@
 package linkedlist;
 
 public class LinkedListBasicOperation {
+    private class Node {
+        int data;
+        Node next;
+    }
+
     private Node head;
     private Node tail;
     private int size;
+
 
     public static void main(String[] args) throws Exception {
         LinkedListBasicOperation ls = new LinkedListBasicOperation();
@@ -12,10 +18,16 @@ public class LinkedListBasicOperation {
         ls.insertAtFirst(30);
         ls.insertAtFirst(40);
         ls.display();
-        System.out.println();
         System.out.println(ls.getNodeAt(0).data);
-        System.out.println(ls.getFirst());
-        System.out.println(ls.getLast());
+        System.out.println("Fisrt value ---->"+ls.getFirst());
+        System.out.println("Last Value ---->"+ls.getLast());
+        System.out.println("Value at index----->"+ls.getAt(2));
+        System.out.println("Remove First ---->"+ls.removeFirst());
+        System.out.println("Remove First ---->"+ls.removeFirst());
+        System.out.println("Remove First ---->"+ls.removeFirst());
+//        System.out.println("Remove First ---->"+ls.removeFirst());
+//        System.out.println("Remove First ---->"+ls.removeFirst());
+        ls.display();
 
     }
 
@@ -35,10 +47,13 @@ public class LinkedListBasicOperation {
 
     public void display() {
         Node temp = this.head;
+        System.out.println("-----------------------------");
         while (temp != null) {
             System.out.print(temp.data + " ");
             temp = temp.next;
         }
+        System.out.println();
+        System.out.println("-----------------------------");
     }
 
     public void insertAtLast(int value) {
@@ -89,10 +104,35 @@ public class LinkedListBasicOperation {
         return temp;
     }
 
-    private class Node {
-        int data;
-        Node next;
+    public int getAt(int index) throws Exception {
+        if (this.size == 0) {
+            throw new Exception("LinkedList is empty");
+        }
+        if (index < 0 || index >= size) {
+            throw new Exception("Invalid index");
+        }
+        Node temp = this.head;
+        for (int i = 1; i <= index; i++) {
+            temp = temp.next;
+        }
+        return temp.data;
     }
 
+    public int removeFirst() throws Exception {
+        if (this.size==0){
+            throw new Exception("LL is empty");
+        }
+        int firstValue=this.head.data;
+
+        if (size==1){
+            this.head=null;
+            this.tail=null;
+            this.size=0;
+        }else {
+            this.head=this.head.next;
+            size--;
+        }
+        return firstValue;
+    }
 
 }
